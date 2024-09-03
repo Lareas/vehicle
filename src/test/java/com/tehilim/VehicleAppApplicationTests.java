063dd9ec454a22195f0613bef11040d2270ae42d
Luiz Fernando Areas <lfernandoareas@gmail.com>
2024-09-03 10:10:33 -0300

    first commit main

---
+++ src/test/java/com/tehilim/VehicleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tehilim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
